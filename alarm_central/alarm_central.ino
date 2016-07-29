1c9275071992d30502075cfa95e6861992525bde
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-07-29 14:58:50 -0400

    Removing the static control variable and beggining the addition of the SD library and SD controlcodes storage Data

--- alarm_central/alarm_central.ino
+++ alarm_central/alarm_central.ino
@@ -1,39 +1,100 @@
+//Adding the external libraries that are needed
+/*
+* SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4
+*/
+#include <SD.h>
+#include <SPI.h>
 #include <RCSwitch.h>
 //By default, the RF SENSOR pin is definited in RFremote.h on pin2
+/*
+  @TODO
+  A function that defines manually the pins
+*/
 #define SENSOR_PIR1 3
-#define SENSOR_PIR2 4
+#define SDCARD 4
 #define NEW_CONTROL_BUTTON 5 
 #define SIREN 8
 #define RED_LED 10
 #define GREEN_LED 9
 #define INDEFINIDO -1
+
+//Defining all global scope variables
+
+//The timer to use on Blinks.
 unsigned long previousMillis = 0;
+//The state control variable
 int state;
+//The possible status enum
 enum Status {
     ALARM_OFF,
     ALARM_ON,
     ALARM_STARTED,
     NEW_CONTROL_ADDING
   };
-
+  
+//All the possible received signals
 enum receivedSignal {
     CONTROL_SIGNAL,
     SENSOR_SIGNAL,
     NEW_CONTROL_BUTTON_PRESSED
   };
 
-long int controle = 156108005;
+//Define the file containing all control codes
+File myFile;
 
+//TODO Auto control codes generation
+//Here you put the quantity of controls that you want in you 
+long int controles [21];
 
+//Defining the RF433Mhz decoder library
 RCSwitch mySwitch = RCSwitch();
 
 void setup() {
+  
   initiatePins();
   state = ALARM_OFF;
   Serial.begin(9600);
   Serial.println("INICIADO!");
+  //If the card isn't located the software will get into sleep mode.
+  if (!SD.begin(SDCARD)) {
+    Serial.println("Initialization Failed! Please verify your SD Card and try Again");
+    digitalWrite(RED_LED, HIGH);
+    return;
+  }
   mySwitch.enableReceive(0);
+  myFile = SD.open("codes.txt", FILE_WRITE);
 
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    myFile.println("testing 1, 2, 3.");
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+  // re-open the file for reading:
+  myFile = SD.open("codes.txt");
+  if (myFile) {
+    Serial.println("codes.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
 }
 
 void loop() {
@@ -90,7 +151,6 @@ void loop() {
 
 void initiatePins() {
     pinMode(SENSOR_PIR1, INPUT);
-    pinMode(SENSOR_PIR2, INPUT);
     pinMode(NEW_CONTROL_BUTTON, INPUT_PULLUP);
     pinMode(SIREN, OUTPUT);
     pinMode(RED_LED, OUTPUT);
@@ -101,25 +161,24 @@ int receivedSignal() {
        if (mySwitch.available()) {
               Serial.println(mySwitch.getReceivedValue());
               Serial.println();
-              if (controle == mySwitch.getReceivedValue()) {
-                Serial.println("Control Signal");
-                mySwitch.resetAvailable();
-                return CONTROL_SIGNAL; 
+              for (int i=0; i < sizeof(controles); i++) {
+                  if (controles[i] == mySwitch.getReceivedValue()) {
+                  Serial.println("Control Signal");
+                  mySwitch.resetAvailable();
+                  return CONTROL_SIGNAL; 
+                }
               }
               mySwitch.resetAvailable();
           }
 //     if (digitalRead(SENSOR_PIR1) == 0) {
 //           return SENSOR_SIGNAL; 
 //        }
-//      if (digitalRead(SENSOR_PIR2) == 0) {        
-//        Serial.println("Sensor PIR2 Signal");        
-//        return SENSOR_SIGNAL;      
-//      }
     if (digitalRead(NEW_CONTROL_BUTTON) == 0) {
         return NEW_CONTROL_BUTTON_PRESSED;
     }
     return INDEFINIDO;
 }
+
 void ledBlink(int led, int speed_milis) {
    int state = digitalRead(led);
    const long interval = speed_milis; 
@@ -129,17 +188,21 @@ void ledBlink(int led, int speed_milis) {
       digitalWrite(led, !state);
     }
 }
+
 void turnOn(int pin) {
   digitalWrite(pin, HIGH);
 }
+
 void turnOff(int pin) {
   digitalWrite(pin, LOW);
 }
+
 void sirenBeep(int times) {
       turnOn(SIREN);
       delay(times * 300);
       turnOff(SIREN);  
 }
+
 void setAlarmOn() {
     turnOff(GREEN_LED);
     Serial.println("Alarm On");
@@ -149,6 +212,7 @@ void setAlarmOn() {
     delay(300);
     turnOff(GREEN_LED);  
 }
+
 void setNewControllAddingState() {
       state = NEW_CONTROL_ADDING;
       Serial.println("New Control Adding");
@@ -177,9 +241,11 @@ void setAlarmOff() {
     sirenBeep(2);
     turnOff(RED_LED);
 }
+
 void startAlarm() {
     state = ALARM_STARTED;  
     Serial.println(state);
     Serial.println("Alarm STARTED");
     turnOn(SIREN);
 }
+
